22446537fffca1c93a386c3756b7da5ed58431da
YNP. Jayuda <yulius.Jayuda@gmail.com>
2025-08-15 23:24:42 +0700

    ok

--- src/window_egui.rs
+++ src/window_egui.rs
@@ -4367,7 +4367,7 @@ FROM sys.dm_exec_sessions ORDER BY cpu_time DESC;".to_string()
                                 .hint_text("Enter SQL WHERE condition (e.g., column1 = 'value' AND column2 > 100)")
                         );
                         
-                        if ui.button("🔄").on_hover_text("Apply filter").clicked() || (filter_response.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter))) {
+                        if filter_response.lost_focus() || ui.input(|i| i.key_pressed(egui::Key::Enter)) {
                             self.apply_sql_filter();
                         }
                         
